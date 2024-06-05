b16a75dfc68b46ddd17354148fc51779cef00a6b
Mateus Antonio <128169979+MasuetASF@users.noreply.github.com>
2024-06-05 17:45:29 -0300

    Adicionando meu primeiro programa de Java

---
+++ olaMundo/src/olaMundo/OlaMundo.java
@@ -0,0 +1,16 @@
+package olaMundo;
+
+public class OlaMundo {
+	public static void main(String[] args) {
+		int numero = 1;
+		char caracter = 'a';
+		float numeroFlutuante = 123.12F;
+		double numeroComCasaDecimal = 3.1;
+		String texto = "Teste";
+		
+		
+		
+		System.out.println("Olá mundo");
+	}
+	
+}
