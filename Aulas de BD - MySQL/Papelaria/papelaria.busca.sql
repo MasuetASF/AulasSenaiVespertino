c9f592c9febba5be361f92a0fa6a87add3492975
Mateus Antonio <mateus.antonio18@hotmail.com>
2024-07-08 17:26:59 -0300

    Adicionado novas queries do MYSQL

---
+++ Aulas de BD - MySQL/Papelaria/papelaria.busca.sql
@@ -0,0 +1,22 @@
+-- Quero descobrir, qual o fornecedor teve mais produtos vendidos?
+
+SELECT NOMEFUNCIONARIO,MATRICULA 
+	FROM FUNCIONARIO 
+		WHERE FUNCAO = 'Vendedor';
+        
+select f.MATRICULA, F.NOMEFUNCIONARIO, F. FUNCAO
+	FROM FUNCIONARIO F LEFT JOIN VENDAS V
+        ON F.MATRICULA = v.MATRICULA
+        WHERE V.MATRICULA IS NULL;
+        
+	
+SELECT P.NOMEPRODUTO AS 'NOMEPRODUTO', SUM(P.PRECO * LP.QNTVENDIDO) AS 'TOTAL',
+	SUM(LP.QNTVENDIDO) AS 'QUANTIDADE'
+		FROM PRODUTO P INNER JOIN LISTAPRODUTOS LP
+			ON P.CODIGO = LP.CODIGO
+				GROUP BY P.NOMEPRODUTO;
+                
+                
+                
+                
+                
