c254cbbc3d507e272291464b846076e809e5a07f
Mateus Antonio <mateus.antonio18@hotmail.com>
2024-07-11 15:20:32 -0300

    aadasdsasfaf

---
+++ api/src/main/java/br/com/api/api/repositorio/PessoaRepositorio.java
@@ -0,0 +1,10 @@
+package br.com.api.api.repositorio;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.com.api.api.modelo.Pessoa;
+
+
+public interface PessoaRepositorio extends CrudRepository<Pessoa, Integer> {
+
+}
